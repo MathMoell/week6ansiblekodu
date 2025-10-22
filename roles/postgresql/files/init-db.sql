2c41f7ddf7c1083e39c61a8511a2377bc05974b9
MathMoell <mathias.moll@hkhk.edu.ee>
2025-10-23 01:03:46 +0300

    first commit

---
+++ roles/postgresql/files/init-db.sql
@@ -0,0 +1,7 @@
+-- This file contains SQL commands for initializing the database. 
+
+CREATE DATABASE my_database;
+
+CREATE USER my_user WITH ENCRYPTED PASSWORD 'my_password';
+
+GRANT ALL PRIVILEGES ON DATABASE my_database TO my_user;
